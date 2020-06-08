20ecd732e888b91636affeee97056acd984cb613
Ray Celona <rayc7780@gmail.com>
2020-06-08 13:38:23 -0400

    Basic app/server updates

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,9 @@
 const express = require('express');
+
+const app = express();
+
+app.use((req,res, next) => {
+    res.send('express yourself');
+});
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,1 +1,6 @@
 const http = require('http');
+const app = require('./backend/app');
+
+const server = http.createServer(app);
+
+server.listen(process.env.PORT || 3000)
