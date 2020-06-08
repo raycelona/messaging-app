728569e13694463aab40651bca9182e63ae8c0b0
Ray Celona <rayc7780@gmail.com>
2020-06-08 15:48:18 -0400

    Node, express, HTML updates

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,13 @@ const express = require('express');
 
 const app = express();
 
+// app.use((req, res, next) => {
+//     console.log('first');
+//     next();
+// });
+
 app.use((req,res, next) => {
-    res.send('express yourself');
+    res.send('Express yourself');
 });
 
 module.exports = app;
